8a22c2c3ef43af431864bbeb18c9ecb46c8cc394
Samuel Bernhardt <sdbernhardt@gmail.com>
2019-05-04 12:12:34 -0400

    Removed unused dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 var gulp = require('gulp');
 var bookmarklet = require('gulp-bookmarklet');
-var rename = require('gulp-rename');
-var generateTemplate = require('gulp-template-generator');
-var sass = require('gulp-sass');
 
 gulp.task('default', function(callback) {
   gulp.watch(['src/bookmarklet.js'], ['scripts']);
@@ -13,10 +10,4 @@ gulp.task('scripts', function() {
   gulp.src('src/bookmarklet.js')
     .pipe(bookmarklet())
     .pipe(gulp.dest('dist/'))
-})
-
-gulp.task('styles', function() {
-  gulp.src('server/styles/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('server/styles/'))
 })
